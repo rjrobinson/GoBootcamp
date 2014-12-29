49e51972f3dfb6d045596078f49f24db1255061c
RJ Robinson <robertj.robinson@gmail.com>
2014-12-29 12:02:27 -0500

    json and nethttp imports

---
+++ thebasics.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	client := &http.Client{}
+	resp, err := client.Get("http://gobootcamp.com")
+
+	fmt.Println(resp)
+	fmt.Println(err)
+
+}
